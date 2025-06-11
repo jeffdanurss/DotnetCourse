8183253397496dd5591c7feb6a1e77e558a3fdd1
Jefferson Llerena <150809369+jeffdanurss@users.noreply.github.com>
2025-06-11 13:04:30 -0500

    first commit: first UnitTest.

---
+++ Wpm.Management.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Wpm.Management.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Pet_debe_ser_igual()
+    {
+        var id = Guid.NewGuid();
+
+        var pet1 = new Pet() { Id = id};
+        var pet2 = new Pet() { Id = id};
+
+        Assert.True(pet1 == pet2);
+
+    }
+}
---
+++ Wpm.Management.Domain/Pet.cs
@@ -0,0 +1,7 @@
+﻿namespace Wpm.Management.Domain;
+
+public class Pet : Entity
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
